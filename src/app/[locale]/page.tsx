9fa7a7c872a3883125f4e1de9c3250600688cae5
Guilherme Bustamante <guilherme_bustamante@live.com>
2024-02-23 15:48:04 -0300

    feat: add initial hero section content

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,15 @@
+import HeroSection from "@/sections/hero-section";
 import { useTranslations } from "next-intl";
 
 export default function Index() {
   const t = useTranslations("Index");
   return (
     <main>
-      <div className="flex flex-col">
-        <h1>{t("title")}</h1>
+      <HeroSection />
+      <div className="mx-auto w-full max-w-7xl p-5">
+        <div className="flex flex-col">
+          <h1>{t("title")}</h1>
+        </div>
       </div>
     </main>
   );
---
+++ src/sections/hero-section.tsx
@@ -0,0 +1,61 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import { FaLinkedinIn, FaWhatsapp, FaGithub } from "react-icons/fa6";
+import { FaMailBulk } from "react-icons/fa";
+import { IoChevronDownCircleOutline } from "react-icons/io5";
+
+export default function HeroSection() {
+  return (
+    <section>
+      <div className=" mx-auto flex w-full flex-col items-center justify-center md:relative xl:max-w-7xl">
+        <Image
+          src="/guibusTextBanner.svg"
+          alt="Guibus Banner"
+          width={0}
+          height={0}
+          className="relative h-auto w-full object-cover xl:rounded-b-xl"
+          sizes="100vw"
+          priority
+          draggable="false"
+        />
+
+        <div className="my-3 flex md:absolute md:bottom-3 md:right-3 md:my-0">
+          <div className="flex items-center justify-center gap-2 md:flex-col">
+            <Button size={"icon"} variant={"outline"} asChild>
+              <Link
+                href={"https://www.linkedin.com/in/gui-bus/"}
+                target="_blank"
+              >
+                <FaLinkedinIn size={20} />
+              </Link>
+            </Button>
+
+            <Button size={"icon"} variant={"outline"} asChild>
+              <Link href={"https://github.com/gui-bus"} target="_blank">
+                <FaGithub size={20} />
+              </Link>
+            </Button>
+
+            <Button size={"icon"} variant={"outline"} asChild>
+              <Link href={"mailto:guibus.dev@gmail.com"} target="_blank">
+                <FaMailBulk size={20} />
+              </Link>
+            </Button>
+
+            <Button size={"icon"} variant={"outline"} asChild>
+              <Link href={"https://wa.me/12981847553"} target="_blank">
+                <FaWhatsapp size={20} />
+              </Link>
+            </Button>
+          </div>
+        </div>
+
+        <IoChevronDownCircleOutline
+          className="absolute bottom-3 hidden animate-bounce text-white md:block"
+          size={30}
+        />
+      </div>
+    </section>
+  );
+}
