28013f7fcd4760339fd3dc86e28341d74541adbd
Guilherme Bustamante <guilherme_bustamante@live.com>
2024-02-27 12:50:22 -0300

    style: add defaultTheme=dark

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
       <body className={montserrat.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
